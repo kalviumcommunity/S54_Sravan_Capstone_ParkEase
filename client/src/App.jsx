8b8fe489e93be6ca12b623edacd9964b32a1b99e
sravanr788 <sravanr7882gmail.com>
2024-03-20 16:03:49 +0530

    added routes and context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AllRoutes from './routes/AllRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>
-        Welcome to Frontend of Parkease
-      </h1>
+      <AllRoutes />
     </>
   )
 }
---
+++ client/src/routes/AllRoutes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../components/HomePage";
+import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
+// import Navbar from '../components/Navbar';
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/nav" element={<Navbar />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
