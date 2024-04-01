cc4b05ec36c3f055955e201bd77184fed08fea2d
sravanr788 <sravanr7882gmail.com>
2024-04-01 09:52:03 +0530

    added put route

--- backend/routes/Spaces.js
+++ backend/routes/Spaces.js
@@ -1,10 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { getSpace , getAllSpaces , createSpace } = require("../controllers/SpaceController")
+const { getSpace , getAllSpaces , createSpace , updateSpace } = require("../controllers/SpaceController")
 
 router.get("/all", getAllSpaces)
 router.get("/:id", getSpace)
 router.post("/", createSpace)
-
+router.put("/:id",updateSpace)
 module.exports = router
