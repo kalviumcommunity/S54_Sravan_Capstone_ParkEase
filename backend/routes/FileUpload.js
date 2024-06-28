d84fefdac1f043866ae000b5c4a98b9455aab954
Arjun Kotha <141010334+Phoenix1531@users.noreply.github.com>
2024-06-28 22:20:31 +0530

    server-fix

--- backend/routes/FileUpload.js
+++ backend/routes/FileUpload.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/multerSetup"); // Correctly require the upload configuration
 const { uploadFile } = require("../controllers/imageUploadController");
+const { limiter } = require("../rateLimiter");
 
 // File upload route
 // router.post("/upload", upload.single("file"), uploadFile);
-router.post("/create", upload.array("files"), uploadFile);
+router.post("/create",limiter, upload.array("files"), uploadFile);
 
 module.exports = router;
